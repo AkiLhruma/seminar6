95005efecb613dae5ef95b7ab265deeb9cd1a347
Aki <arishok.kun1009@gmail.com>
2022-10-14 18:58:05 +1000

    this time for shure2

--- Program.cs
+++ Program.cs
@@ -8,13 +8,13 @@
 
 int[] CreateArray(int size, int minValue, int maxValue)
 {
-int[] newArray = new int[size];
+    int[] newArray = new int[size];
 
-for(int i = 0; i < size; i++)
-{
-    newArray[i] = new Random().Next(minValue, maxValue);
-}
-return newArray;
+    for(int i = 0; i < size; i++)
+    {
+        newArray[i] = new Random().Next(minValue, maxValue);
+    }
+    return newArray;
 }
 
 /*Напишите программу замены элементов массива: положительные 
